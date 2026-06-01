e64c14861fa11e04d486b6ca6547c0f46380b3d5
Faraz-cell21 <faraz127200@outlook.com>
2026-06-01 16:35:47 +0500

    added system health route and check, implemented app and server file

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import compression from 'compression';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.use(compression());
+
+app.use(express.json());
+
+app.use('/api/v1', routes);
+
+export default app;
---
+++ src/config/env/env.schema.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const envSchema = z.object({
+    NODE_ENV: z.enum(['development', 'production', 'test']),
+    PORT: z.string(),
+});
+
+export type ENV = z.infer<typeof envSchema>
